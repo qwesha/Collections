4720bc76411ad11d7aec49cc2df33422f4a23dec
geo <joraapple@icloud.com>
2025-04-02 21:50:58 +0300

    first commit

---
+++ src/test/java/ru/aston/collections/CollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aston.collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
